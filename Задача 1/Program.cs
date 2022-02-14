ad25357c9996ab41fa960086d09f4d9ac84733f3
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2022-02-14 10:30:12 +0700

    Update Program.cs

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -2,16 +2,16 @@ using System;
 namespace Basic_concepts_C_Sharp {
     class Program {
         static void Main(string[] args) {
-            int n, a, Result;
-            Console.Write("Введите число a: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите число n: ");
-            n = Convert.ToInt32(Console.ReadLine());
-            Result = a;
-            for (int Step = 1; Step < n; ++Step) {
-                Result *= a;
+            int Degree, Input, Result;
+            Console.Write("Введите число: ");
+            Input = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите степень числа: ");
+            Degree = Convert.ToInt32(Console.ReadLine());
+            Result = Input;
+            for (int Step = 1; Step < Degree; ++Step) {
+                Result *= Input;
             }
-            Console.WriteLine(result);
+            Console.WriteLine(Result);
         }
     }
 }
